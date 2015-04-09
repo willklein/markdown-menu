8bcc9b6ec725aab028e5fb927aa6ed389da94297
Will Klein <will.klein@gmail.com>
2015-04-08 23:28:58 -0600

    Refactor contentscript into functions

--- chrome/app/scripts/contentscript.js
+++ chrome/app/scripts/contentscript.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var header, tag, headerLevelStr, depth;
-var headers = document.querySelectorAll('article.markdown-body h1, article.markdown-body h2, article.markdown-body h3, article.markdown-body h4, article.markdown-body h5, article.markdown-body h6');
-var links = [];
+var getLinks = function() {
+  var header, tag, headerLevelStr, depth;
+  var headers = document.querySelectorAll('article.markdown-body h1, article.markdown-body h2, article.markdown-body h3, article.markdown-body h4, article.markdown-body h5, article.markdown-body h6');
+  var links = [];
 
-for (var i = 0; i < headers.length; ++i) {
-  header = headers[i];
-  tag = header.tagName;
-  headerLevelStr = tag.substring(1);
-  depth = Number.parseInt(headerLevelStr);
+  for (var i = 0; i < headers.length; ++i) {
+    header = headers[i];
+    tag = header.tagName;
+    headerLevelStr = tag.substring(1);
+    depth = Number.parseInt(headerLevelStr);
 
-  links.push({
-    text: header.innerText,
-    hash: header.children[0].hash,
-    depth: depth
-  });
-}
+    links.push({
+      text: header.innerText,
+      hash: header.children[0].hash,
+      depth: depth
+    });
+  }
+
+  return links;
+};
 
 var openList = function(depth) {
   var html = '';
@@ -35,48 +39,57 @@ var closeList = function(depth) {
   }
 
   return html;
-
 };
 
-var node;
-var toc = '<ul>';
-var currentDepth = 1;
+var buildContents = function(links) {
+  var node;
+  var contents = '<ul>';
+  var currentDepth = 1;
 
-for (i = 0; i < links.length; ++i) {
-  node = links[i];
+  for (var i = 0; i < links.length; ++i) {
+    node = links[i];
 
-  if (node.depth > currentDepth) {
-    toc += openList(node.depth - currentDepth);
-  } else if (node.depth < currentDepth) {
-    toc += closeList(currentDepth - node.depth);
+    if (node.depth > currentDepth) {
+      contents += openList(node.depth - currentDepth);
+    } else if (node.depth < currentDepth) {
+      contents += closeList(currentDepth - node.depth);
+    }
+
+    currentDepth = node.depth;
+
+    contents += '<li><a href="' + node.hash + '">' + node.text + '</a></li>';
   }
 
-  currentDepth = node.depth;
+  while (currentDepth--) {
+    contents += '</ul>';
+  }
 
-  toc += '<li><a href="' + node.hash + '">' + node.text + '</a></li>';
-}
+  return contents;
+};
 
-while (currentDepth--) {
-  toc += '</ul>';
-}
+var insertContents = function(contents) {
+  var fileView = false;
+  var readmeTarget = document.querySelectorAll('#readme > h3')[0];
+  var markdownTarget;
 
-var fileView = false;
-var target = document.querySelectorAll('#readme > h3')[0];
+  if (!readmeTarget) {
+    markdownTarget = document.querySelectorAll('.file-actions')[0];
+  }
 
-if (!target) {
-  target = document.querySelectorAll('.file-actions')[0];
-  fileView = true;
-}
+  if (!readmeTarget && !markdownTarget) {
+    return false;
+  }
 
-if (target) {
   var oldLink = document.querySelectorAll('.github-markdown-contents')[0];
 
   if (oldLink) {
     oldLink.parentNode.removeChild(oldLink);
   }
 
-  var link = '<span class="github-markdown-contents select-menu js-menu-container js-select-menu"><span class="github-markdown-contents-btn js-select-menu js-menu-target btn btn-sm tooltipped-s' + (fileView ? '' : ' right') + '" role="button" aria-label="Show Table of Contents"><span class="octicon octicon-three-bars"></span></span><div class="select-menu-modal-holder github-markdown-contents-modal-holder js-menu-content js-navigation-container"><div id="github-markdown-contents-container" class="select-menu-modal"></div></div></span>';
-  target.innerHTML += link;
+  var link = '<span class="github-markdown-contents select-menu js-menu-container js-select-menu"><span class="github-markdown-contents-btn js-select-menu js-menu-target btn btn-sm tooltipped-s' + (markdownTarget ? '' : ' right') + '" role="button" aria-label="Show Table of Contents"><span class="octicon octicon-three-bars"></span></span><div class="select-menu-modal-holder github-markdown-contents-modal-holder js-menu-content js-navigation-container"><div id="github-markdown-contents-container" class="select-menu-modal">' + contents + '</div></div></span>';
+  (readmeTarget || markdownTarget).innerHTML += link;
+};
 
-  document.getElementById('github-markdown-contents-container').innerHTML += toc;
-}
+var links = getLinks();
+var contents = buildContents(links);
+insertContents(contents);
